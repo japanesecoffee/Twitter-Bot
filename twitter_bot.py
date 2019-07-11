afdc81e199c2765fc3f449dc51d4622b4feefca1
Jason <kangedklan@gmail.com>
2019-07-11 00:30:51 -0400

    set up Twitter api access

---
+++ twitter_bot.py
@@ -0,0 +1,14 @@
+import tweepy
+
+print("testing bot")
+
+#assigning Twitter keys to variables
+CONSUMER_KEY = "CbQBaHTweTSxOb2ss0pyx3am4"
+CONSUMER_SECRET = "wFhobn6SRuXl052vtBbr0KWsOJ8LjMPXHVLRbHfFFT3gCZZx4b"
+ACCESS_KEY = "1148796843553435651-fiIeADuxfmJlpMEtDVJs0NmT1XhGUI"
+ACCESS_SECRET = "zvGVdz1CHOv27PKvI99HE2vml8NYH2N4h0BMSiZNbowpa"
+
+#using api object to communicate with Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
