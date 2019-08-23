c61c4d42bb93dc7972533df2b2ca8b318b46e146
Jason <kangedklan@gmail.com>
2019-08-23 17:21:36 -0400

    removed keys and generated new keys

---
+++ sample_keys.py
@@ -0,0 +1,6 @@
+#assigning Twitter keys to variables
+#hid my real api keys in a separate file
+CONSUMER_KEY = "1234ASDF"
+CONSUMER_SECRET = "1234ASDF"
+ACCESS_KEY = "9876FDSA"
+ACCESS_SECRET = "9876FDSA"
--- twitter_bot.py
+++ twitter_bot.py
@@ -1,15 +1,12 @@
 import tweepy
 import time
-
-#assigning Twitter keys to variables
-CONSUMER_KEY = "CbQBaHTweTSxOb2ss0pyx3am4"
-CONSUMER_SECRET = "wFhobn6SRuXl052vtBbr0KWsOJ8LjMPXHVLRbHfFFT3gCZZx4b"
-ACCESS_KEY = "1148796843553435651-fiIeADuxfmJlpMEtDVJs0NmT1XhGUI"
-ACCESS_SECRET = "zvGVdz1CHOv27PKvI99HE2vml8NYH2N4h0BMSiZNbowpa"
+#removed keys and generated new keys in keys.py
+#keys.py added to gitignore, so view sample_keys.py to see format
+import keys
 
 #using api object to communicate with Twitter
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
 api = tweepy.API(auth)
 
 #this text file stores the id of the last tweet responded to
